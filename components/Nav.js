76edac14cb70b1147e4cbafd9e1d2a6bf7836100
thirumeniram Arumugam <thirumeniram25@gmail.com>
2024-04-07 01:05:43 +0530

    fifth commit

--- components/Nav.js
+++ components/Nav.js
@@ -14,6 +14,8 @@ const Navbar = () => {
   
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">E-shop</span>
         </a>
+
+        
         
         <button onClick={() => setIsMenuOpen(!isMenuOpen)} data-collapse-toggle="navbar-hamburger" type="button" className="md:hidden inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded={isMenuOpen}>
           <span className="sr-only">Open main menu</span>
@@ -23,9 +25,7 @@ const Navbar = () => {
         </button>
         <div className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden w-full`} id="navbar-hamburger">
           <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-          <li>
-              <a href="../" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Home</a>
-            </li>
+        
             <li>
               <a href="../Settings" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Settings</a>
             </li>
